9e89ab1e2dffac259cc1fff5572471e7283ba3e5
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-06-12 21:22:06 +0800

    update translation

--- home_1.php
+++ home_1.php
@@ -254,7 +254,15 @@
                 <div class="translate" id="google_translate_element"></div>
 
                     <script type="text/javascript">
-                        function googleTranslateElementInit() {  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}
+                        function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({
+                            pageLanguage: 'en',
+                            includedLanguages: 'en,tl',
+                            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                            autoDisplay: false,
+                            multilanguagePage: true
+                        }, 'google_translate_element');
+                        }
                     </script>
                     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
             </span>
