829846be3fcfe84589dccbea7588e2057237829d
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-06-12 21:21:09 +0800

    update translation

--- forgot_pass_4_reset_pass.php
+++ forgot_pass_4_reset_pass.php
@@ -75,9 +75,17 @@
             <span>
                 <div class="translate" id="google_translate_element"></div>
                 <script type="text/javascript">
-                    function googleTranslateElementInit() {  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}
-                </script>
-                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                        function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({
+                            pageLanguage: 'en',
+                            includedLanguages: 'en,tl',
+                            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                            autoDisplay: false,
+                            multilanguagePage: true
+                        }, 'google_translate_element');
+                        }
+                    </script>
+                    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
             </span>
             <!--<div class="dropdown">
                 <form action="#">
