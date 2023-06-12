342f7cacd830d4d787c9626e6a059cd3e3627353
lrnt17 <136178606+lrnt17@users.noreply.github.com>
2023-06-12 15:07:16 +0800

    Forgot password tapos na

---
+++ forgot_pass_5_reset_success.php
@@ -0,0 +1,24 @@
+<?php 
+    include("connect.php");
+
+    $pnum=$_SESSION["pnum"];
+    $new_pass = $_POST["con_pass"];
+    $sql=("UPDATE users set user_password = '$new_pass' where user_pnum = '$pnum'");
+    mysqli_query($conn,$sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Changed | SiPa</title>
+</head>
+<body>
+    <div id="reg_block">
+        <h1>Password changed!</h1>
+        <p>Please login to your account again.</p>
+        <a href="login_1.php">Login now</a>
+    </div>
+</body>
+</html>
