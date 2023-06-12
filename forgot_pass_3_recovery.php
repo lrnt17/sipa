5ab8afa2d95a87772806a8227767ed5c6ddeddef
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-06-12 21:20:27 +0800

    update translation

--- forgot_pass_3_recovery.php
+++ forgot_pass_3_recovery.php
@@ -45,10 +45,18 @@
             <!-- language -->
             <span>
                 <div class="translate" id="google_translate_element"></div>
-                <script type="text/javascript">
-                    function googleTranslateElementInit() {  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');}
-                </script>
-                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+               <script type="text/javascript">
+                        function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({
+                            pageLanguage: 'en',
+                            includedLanguages: 'en,tl',
+                            layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                            autoDisplay: false,
+                            multilanguagePage: true
+                        }, 'google_translate_element');
+                        }
+                    </script>
+                    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
             </span>
             <a href="">Privacy Policy</a>
             <a href="">Terms of Use</a>
