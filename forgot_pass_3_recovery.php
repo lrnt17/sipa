99c79ace193f1b1f073ae9727c809749792879ac
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-06-12 22:45:02 +0800

    update translation - ala na topbar

--- forgot_pass_3_recovery.php
+++ forgot_pass_3_recovery.php
@@ -12,6 +12,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Recovery | SiPa</title>
+    <style>
+   .skiptranslate iframe  {
+visibility: hidden !important;
+    } 
+body{
+    top:0!important;
+    
+}</style>
 </head>
 <body>
     <div class="parent">
