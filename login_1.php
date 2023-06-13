f19c67cb949ee34b92da69d299d12a6e29c7a76b
mika <mika.acunaa@gmail.com>
2023-06-14 00:17:45 +0800

    add css, hindi pa responsive

--- login_1.php
+++ login_1.php
@@ -7,51 +7,243 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign in | SiPa</title>
+
 <style>
    .skiptranslate iframe  {
     visibility: hidden !important;
     } 
+
     body{
-    top:0!important;
+        top:0!important;
+        background: linear-gradient(67deg,#70AFED ,#1F6CB5 25%);
+        font-family: Helvetica Neue;
+	    scroll-behavior: smooth;
+        letter-spacing: 1px;
     }
- </style>
-</head>
+
+    .parent{
+        margin: 0;
+        padding: 0;
+        width: 100%;
+    }
+    
+    .container{
+        
+    }
+
+    .text{
+        margin-top: 80px;
+        padding-left: 70px;
+    }
+
+    p{
+        white-space: nowrap;
+        
+    }
+
+    .header-text{
+        font-size: 50px;
+        color:#FFFFFF;
+        font-weight: 300;
+        margin-top: -50px;
+    }
+
+    .header-text2{
+        font-size: 50px;
+        color:#FFFFFF;
+        margin-top: -50px;
+        font-weight: 500;
+    }
+
+    .info-text{
+        font-size: 20px;
+        color:#FFFFFF;
+        margin-top: -40px;
+        font-weight: 300;
+    }
+
+    .logo{
+        height: 65px;
+        cursor: pointer;
+        padding: 20px 50px 20px;
+    }
+
+    .doctor{
+        height: 470px;
+	    position: absolute;
+        bottom: 0;
+        padding-left: 270px;
+        z-index: -1;
+    }
+
+    .sign-in{
+        width: 450px;
+        min-height: 480px;
+        background: #F4F7FF;
+        border-radius: 40px;
+        box-shadow: 0 0 5px rgba(0,0,0,.3);
+        padding: 20px 40px;
+    }
+
+    .container-box{
+        right: 0px;
+        margin-top: -170px;
+        float: right;
+        padding-right: 50px;
+    }
+
+    .container-box .sign-in .form input{
+        width: 90%;
+        height: 100%;
+        border: 2px solid #B9B9B9;
+        padding: 13px 20px;
+        font-size: 1rem;
+        border-radius: 0px;
+        background: transparent;
+        outline: none;
+        margin-bottom: 20px;
+    }
+
+    .container-box .sign-in .log-btn{
+        display: block;
+        width: 100%;
+        padding: 13px 20px;
+        text-align: center;
+        border: none;
+        background: #F2C1A7;
+        outline: none;
+        border-radius: 10px;
+        font-size: 1.2rem;
+        color: #FFF;
+        cursor: pointer;
+        margin-bottom: 20px;
+    }
+
+    a {
+        font-weight: 300;
+        text-decoration: none;
+        color: #FFFFFF;
+        letter-spacing: 1px;
+        margin-right:100px;
+    }
+
+    .translate {
+        padding-top: 30px;
+        float: right;
+        z-index: -1;
+    }
+
+    .links{
+        position: absolute;
+        padding-top: 30px;
+        float: right;
+        padding-right: 10px;
+    }
+
+    .checkbox-text{
+        margin: -20px 0px 20px 24px;
+        font-size: 15px;
+        font-weight: 300;
+        color: #575757;
+        
+    }
+    
+    label{
+        font-size: 18px;
+        color: #575757;
+    }
+
+    h1{
+        color: #2F2F2F;
+        font-weight: 500;
+    }
+
+    .a-forgot{
+        float: right;
+        margin-right: 0px;
+        text-decoration: none;
+        color: #404AD0;
+    }
+
+    .text-dha{
+        font-size: 15px;
+        font-weight: 300;
+        text-align: center;
+        color: #575757;
+        padding: 12px;
+    }
+
+    .lines{
+        width: 100%;
+        height: 45px;
+        position: relative;
+    }
+
+    .lines::after, .lines::before{
+        content: '';
+        position: absolute;
+        height: 1px;
+        margin: auto;
+        background: #2F2F2F;
+        width: 25%;
+        top: 45%;
+    }
+
+    .lines::after{
+        left: 0;
+    }
+
+    .lines::before{
+        right: 0;
+    }
+
+    .text-visit{
+        font-size: 15px;
+        font-weight: 400;
+        text-align: center;
+        color: #575757;
+        width:100%;
+    }
+
+</style>
 <body>
 
     <!-- parent div -->
-    <div>
+    <div class="parent">
         <!-- 1st child div -->
-        <div>
-            <img src="" alt=""><!-- background photo-->
-            <img src="" alt=""><!-- logo -->
-            <div><!-- texts -->
-                <p>Welcome back to</p>
-                <h2>SiPa Siguradong Pagpipilian</h2>
-                <p>Find out your top contraceptive methods.</p>
+        <div class = "container">
+            <img class="doctor" src="doctor.png" alt=""><!-- background photo-->
+            <img class="logo" src="logo1.png" alt=""><!-- logo -->
+            <div class = "text"><!-- texts -->
+                <p class = "header-text">Welcome back to</p>
+                <p class = "header-text2">SiPa Siguradong Pagpipilian</p>
+                <p class = "info-text">Find out your top contraceptive methods.</p>
             </div>
         </div>
 
         <!-- 2nd child div -->
-        <div>
+        <div class="container-box">
             <!-- signing in -->
-            <div>
+            <div class = "sign-in">
                 <h1>Sign in</h1>
                 <form action="login_2_successfull.php" method="post">
                     <div class="form">
-                        <input type="text" name="code" id="code" required>
                         <label for="code">Access Code</label>
+                        <input type="text" name="code" id="code" required>
                     </div>
                     <div class="form">
+                    <label for="pass">Password</label>
                         <input type="password" name="pass" id="pass" required>
                         <i class="fas fa-eye" id="togglePassword"></i>
-                        <label for="pass">Password</label>
                     </div>
-                    <input type="submit" value="Sign in" name="login">
-                    <input type="checkbox" name="" id="">Keep me logged in
-                    <a href="forgotpass_1.php" id="forgotpass">Forgot password?</a>
+                    <input class="log-btn" type="submit" value="Sign in" name="login">
+                    <input type="checkbox" name="" id=""><p class ="checkbox-text">Keep me logged in
+                    <a href="forgotpass_1.php" id="forgotpass" class="a-forgot">Forgot password?</a>
                 </form>
-                <p id="dont_have_account">---------- Don't have an account? -------------</p>
-                <p>Visit the nearest health facility in your area</p>
+                <div class="lines">
+                    <p class="text-dha" id="dont_have_account">Don't have an account?</p>
+                </div>
+                <p class="text-visit">Visit the nearest health facility in your area</p>
                 
                 <!-- show password toggle script -->
                 <script>
