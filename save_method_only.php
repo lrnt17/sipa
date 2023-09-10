1f19b3b851f4c1b759077aeb292a4de833cdec96
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-09-10 17:30:38 +0800

    updated code

--- save_method_only.php
+++ save_method_only.php
@@ -10,15 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Escape the data to prevent SQL injection (use mysqli_real_escape_string or prepared statements)
   $selected_method_safe = mysqli_real_escape_string($conn, $selected_method);
 
-  // Update the users table with the selected contraceptive method
-  $query = "UPDATE users SET birth_control_name = '$selected_method_safe', birth_control_startdate = NULL, birth_control_usage = NULL WHERE user_id = '$user_id'";
-  $result = mysqli_query($conn, $query);
+  // First, delete rows from the reminder table with the same user_id
+  $deleteQuery = "DELETE FROM reminder WHERE user_id = '$user_id'";
 
-  // Check if the update was successful
-  if ($result) {
-    echo json_encode(['success' => true]);
+  // Execute the delete query
+  if (mysqli_query($conn, $deleteQuery)) {
+    // The rows in the reminder table have been deleted successfully
+    // Now, update the users table with the selected contraceptive method
+    $updateQuery = "UPDATE users SET birth_control_name = '$selected_method_safe', birth_control_startdate = NULL, birth_control_enddate = NULL, birth_control_usage = NULL, isMessaged = NULL WHERE user_id = '$user_id'";
+    $result = mysqli_query($conn, $updateQuery);
+
+    // Check if the update was successful
+    if ($result) {
+      echo json_encode(['success' => true]);
+    } else {
+      echo json_encode(['success' => false, 'message' => 'Error updating data in the users table.']);
+    }
   } else {
-    echo json_encode(['success' => false, 'message' => 'Error updating data in the database.']);
+    // Handle the delete query error
+    echo json_encode(['success' => false, 'message' => 'Error deleting data from the reminder table.']);
   }
 
   // Close the database connection
