12a9c301c1ba852dafafa82c8f69208487fe3c4e
yunii25 <134275539+yunii25@users.noreply.github.com>
2023-09-16 13:28:01 +0800

    update datepicker to flatpickr

--- period_calcu.php
+++ period_calcu.php
@@ -9,243 +9,11 @@
     <meta charset="UTF-8">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://kit.fontawesome.com/324d76b648.js" crossorigin="anonymous"></script>
-    <title>Period Tracker</title>
-    <!--<style>
-        body {
-            top:0!important;
-            height: 100%;
-            font-family: 'Inter', sans-serif;
-            scroll-behavior: smooth;
-            letter-spacing: 1px;
-            background: #F2F5FF;
-        }
-
-        #form-container {
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            width:100%;
-        }
-
-        label {
-            display: block;
-            margin-top: 10px;
-            font-size: 15px;
-            color: #575757;
-            text-align:center;
-            font-weight: 500;
-        }
-
-        input {
-            width: 100%;
-            padding: 5px;
-        }
-
-        button {
-            margin-top: 10px;
-            padding: 5px 10px;
-        }
-
-        #result {
-            margin-top: 20px;
-        }
-
-        .calendar-day {
-            display: inline-block;
-            width: 25px;
-            height: 25px;
-            text-align: center;
-            line-height: 25px;
-            margin: 2px;
-        }
-
-        .period-day {
-            background-color: #CAA4D0;
-            color: white;
-        }
-
-        .ovulation-day {
-            background-color: #F0C2A9;
-            color: white;
-        }
-        
-        table {
-          border-collapse: collapse;
-          margin-bottom: 20px;
-        }
-        
-        td{
-            font-size:15px;
-            border: 0px solid #ddd;
-            padding: 10px;
-            text-align:center;
-        }
-
-        th {
-            font-size:15px;
-            border: 0px solid #ddd;
-            padding: 10px;
-            text-align:center;
-            color: #184DA8;
-        }
-
-        .container-header{
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            width:100%;
-            margin: 0;
-        }
-
-        .head{
-            width: 80%;
-            min-height: auto;
-            background: #D2E0F8;
-            border-radius: 30px;
-            box-shadow: 0 0 5px rgba(0,0,0,.3);
-            padding: 40px 40px;
-        }
-
-        h1{
-            display:inline;
-        }
-        
-        .h1text{
-            display: flex;
-            justify-content: center;
-        }
-
-        .header-text{
-            color: #2F2F2F;
-            font-weight: 400;
-            font-size: 60px;
-        }
-
-        .header-text1{
-            color: #2F2F2F;
-            font-weight: 600;
-            font-size: 60px;
-        }
-
-        .infobox{
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            width:100%;
-            margin: 0;
-        }
-
-        .infotext-box{
-            width: 35%;
-            min-height: auto;
-            background: #FFFFFF;
-            border-radius: 25px;
-            box-shadow: 0 0 5px rgba(0,0,0,.3);
-            padding: 5px 70px;
-            margin-top: -2.5%;
-            z-index: 1;
-        }
-
-        p{
-            text-align:center;
-            font-size: 15px;
-            color: #575757;
-        }
-
-        .top-con{
-            padding-left: 7%;
-            padding-top: 3%;
-            display: flex;
-            width:100%;
-            align-items: center;
-
-        }
-
-        .vl {
-            width: 13px;
-            background-color: #1F6CB5;
-            border-radius: 99px;
-            height: 65px;
-        }
-
-        .title{
-            display: flex;
-            justify-content: center;
-        }
-
-        .title-text{
-            margin-left:20px;
-            color: #2F2F2F;
-            font-weight: 600;
-            font-size: 32px;
-        }
-
-        .title-text1{
-            color: #2F2F2F;
-            font-weight: 400;
-            font-size: 32px;
-        }
-
-        .btn{
-            display: flex;
-            justify-content: center;
-            width:100%;
-            margin: 0;
-        }
-
-        .log-btn{
-            width: 25%;
-            padding: 25px 20px;
-            text-align: center;
-            border: none;
-            background: #D2E0F8;
-            border-radius: 25px;
-            font-size: 1.2rem;
-            color: #2F2F2F;
-            cursor: pointer;
-            box-shadow: 0 0 2px rgba(0,0,0,.3);
-            letter-spacing: 1.3px;
-        }
-
-        .form-con{
-            display: flex;
-            width: 80%;
-            min-height: auto;
-            background: #F2F5FF;
-            border-radius: 30px;
-            padding: 40px 40px 40px 0px;
-        }
-
-        .form-box{
-            width: 50%;
-            min-height: auto;
-            padding: 0px 20px 40px 40px;
-            
-        }
-
-        input{
-            width: 85%;
-            height: 50%;
-            border: none;
-            padding: 25px 35px;
-            font-size: 1rem;
-            border-radius: 25px;
-            background: #ffff;
-            outline: none;
-            box-shadow: 0 0 5px rgba(0,0,0,.3);
-            margin-top: 15px;
-        }
-
-        .resultbox{
-            display: flex;
-            justify-content: center;
-            margin: 0 auto;
-            width:100%;
-
-        }
-        
-    </style>-->
+    <title>SiPa | Period Tracker</title>
+    
     <style>
         .period-day {
             background-color: #CAA4D0;
@@ -327,26 +95,7 @@
         </div>
         
     
-    <!--<div id="form-container">
-        <form onsubmit="period_calcu.submit(event)" method="post" class="form-con">
-            <div class="form-box">
-                <label for="last-period">First day of last period</label>
-                <input type="date" id="last-period" required>
-            </div>
-            <div class="form-box">
-                <label for="period-length">Length of last period (in days)</label>
-                <input type="number" id="period-length" placeholder="ex. 7" min="1" max="10" value="5" required>
-            </div>
-            <div class="form-box">
-                <label for="cycle-length">Length of menstrual cycle (in days)</label>
-                <input type="number" id="cycle-length" placeholder="ex. 28" min="21" max="35" value="28" required>
-            </div>
-            <div class="btn">
-                <button class="class_60 log-btn">Calculate my period</button>
-            </div>
-        </form>
-    </div>-->
-
+    
     <div class="text-center"id="form-container">
         <form onsubmit="period_calcu.submit(event)" method="post" class="form-con">
 
@@ -371,8 +120,7 @@
             <div class="row align-items-start my-4">
                 <div class="col">
                     <div class="container p-3 rounded-4 shadow-sm rounded" style="background:#ffff;">
-                        <input class="py-3" type="date" id="last-period" style="border: none;
-                        outline: none; font-size:20px; color:#5A5A5A;"required>
+                    <input class="py-3" type="text" id="last-period" placeholder="Select date" style="border: none; outline: none; font-size:20px; color:#5A5A5A;" required>
                     </div>
                 </div>
                 <div class="col">
@@ -448,6 +196,12 @@
 
     <script>
 
+            // Initialize Flatpickr on the date input element
+        flatpickr("#last-period", {
+            dateFormat: "Y-m-d", // Set the desired date format
+            defaultDate: currentDate, // Set the default date
+        });
+
         var period_calcu = {
 
             numOfMonths: 2,
@@ -512,5 +266,3 @@
     </script>
 </body>
 </html>
-
-
