18553b8a7596ea03071fb9b3f424a7d08e60fc4b
mika <mika.acunaa@gmail.com>
2023-10-03 08:08:47 +0800

    agree to policies css

--- agree-to-policies.php
+++ agree-to-policies.php
@@ -6,13 +6,164 @@
     .cbox{
         width:auto;
     }
+
+    .scroll{
+        padding: 3%;
+            max-height: 40vh; /* Set a maximum height for the container (adjust as needed) */
+            overflow-y: auto; 
+    }
 </style>
 
 <section id="myModal" class="modal">
     <!-- Modal content -->
     <div class="content-modal">
         <span class="close-policies" style="cursor: pointer;">&times;</span>
-        <p>Some text in the Modal..</p>
+        <br>
+        <div class="scroll">
+
+        <h2> <p>Terms and Conditions for SiPa</p> </h2>
+
+       <p> Effective Date: [September 23, 2023] </p>
+
+       <p>These terms and conditions govern your use of SiPa (Siguradong Pagpapaplano).<br>
+        By accessing or using the Website, you agree to abide by these Terms. If you do not <br>
+        agree with these Terms, please refrain from using the Website.</p>
+
+        <h3>1. Information Disclaimer</h3>
+
+        <p>The content provided on this Website is for informational purposes only and should not <br>
+        be considered as medical advice. It is not a substitute for professional medical  <br>
+        consultation.  Consult with a qualified healthcare provider before making any decisions <br>
+        related to contraception or sexual health.</p> 
+
+        <h3>2. User Age Requirement</h3>
+
+        <p>Users must be at least 18 years old to access and use this Website. By using the <br>
+        Website, you confirm that you meet this age requirement.</p> 
+
+        <h3>3. Privacy Policy</h3>
+
+        <p>Please review our Privacy Policy to understand how we collect, use, and protect your <br>
+        personal information when you use this Website. By using the Website, you agree to the <br>
+        terms outlined in the Privacy Policy.</p> 
+
+        <h3>4. User Conduct</h3>
+
+        <p>a. You agree to use the Website in compliance with all applicable laws and regulations.</p>
+        <p>b. You will not engage in any illegal, harmful, or abusive conduct on the Website.</p>
+        <p>c. You will not attempt to gain unauthorized access to the Website or any <br> 
+        related systems. </p> 
+
+        <h3>5. Intellectual Property</h3>
+
+        <p>a. All content and materials on this Website, including text, graphics, logos, and images,<br>
+        are the property of the Company and are protected by intellectual property laws.</p>
+        <p>b. You may not reproduce, distribute, or modify any content from this Website without <br>
+            prior written consent from the Company.</p> 
+
+        <h3>6. Third-Party Links</h3>
+
+        <p>This Website may contain links to third-party websites. These links are provided for <br>
+        convenience only, and the Company does not endorse or take responsibility for the <br>
+        content or practices of these third-party websites.</p> 
+
+        <h3>7. Disclaimer of Warranties</h3>
+
+        <p>a. This Website is provided "as is," without any warranties, express or implied, including <br>
+        but not limited to the accuracy, reliability, or availability of the content.</p>
+        <p>b. The Company disclaims all liability for any errors, omissions, or inaccuracies in the <br>
+        content or for any loss or damage resulting from the use of this Website.</p> 
+
+        <h3>8. Limitation of Liability</h3>
+
+        <p>The Company and its affiliates shall not be liable for any direct, indirect, <br>
+        incidental, consequential, or punitive damages arising from your use or inability to use <br>
+        this Website. </p> 
+
+        <h3>9. Changes to Terms</h3>
+
+        <p>The Company reserves the right to modify these Terms at any time. It is your <br>
+        responsibility to review these Terms periodically for updates. Your continued use of the  <br>
+        Website after any modifications signifies your acceptance of the revised Terms.</p> 
+
+        <h3>10. Termination</h3>
+
+        <p>The Company may terminate or suspend your access to the Website without notice  <br>
+        if you violate these Terms or engage in any conduct that the Company deems harmful <br>
+        or inappropriate.</p> 
+
+        <h3>11. Governing Law</h3>
+
+        <p>These Terms are governed by the laws of Philippines. Any disputes arising from  <br>
+        or related to these Terms shall be subject to the exclusive jurisdiction of the courts </br>
+        in Philippines. </p> 
+
+        <p>By using this Website, you acknowledge that you have read, understood, and agreed to  <br>
+        these Terms and the Privacy Policy. If you have any questions or concerns, <br>
+        please contact us at +63 912 345 6789.</p> 
+
+        <p>Please consult with a legal professional to ensure that these terms and conditions are  <br>
+        appropriate for your specific website and jurisdiction.</p>
+
+        <br><br>
+
+        <h2>Privacy Policy for [SiPa]</h2>
+        <p>Last Updated: [9/23/2023]</p>
+
+        <h3>1. Introduction</h3>
+        <p>Welcome to SiPa (Siguradong Pagpapaplano). We are committed to protecting your <br>
+        privacy and ensuring the security of your personal information. This Privacy Policy  <br>
+        outlines the types of information we collect, how we use it, and how we safeguard your  <br>
+        data when you visit our Website.</p>
+
+
+        <h3>2. Information We Collect</h3>
+        <p>2.1. Personal Information: We may collect personal information that you voluntarily  <br>
+        provide to us when you use our Website. This may include your name, email address,  <br>
+        contact number, and any other information you choose to share with us.</p>
+        
+        <h3>3. Privacy Policy</h3>
+        <p>Please review our Privacy Policy to understand how we collect, use, and protect your  <br>
+        personal information when you use this Website. By using the Website, you agree to the  <br>
+        terms outlined in the Privacy Policy.</p>
+        
+        <h3>4. User Conduct</h3>
+        <p>a. You agree to use the Website in compliance with all applicable laws and regulations.</p>
+        <p>b. You will not engage in any illegal, harmful, or abusive conduct on the Website.</p>
+        <p>c. You will not attempt to gain unauthorized access to the Website or any <br>
+            related systems.</p>
+        
+        <h3>5. Intellectual Property</h3>
+        <p>a. All content and materials on this Website, including text, graphics, logos, and images, <br>
+        are the property of the Company and are protected by intellectual property laws.</p> 
+        <p>b. You may not reproduce, distribute, or modify any content from this Website without <br>
+        prior written consent from the Company.</p>
+        
+        <h3>6. Third-Party Links</h3>
+        <p>This Website may contain links to third-party websites. These links are provided for <br>
+            convenience only, and the Company does not endorse or take responsibility for the <br>
+            content or practices of these third-party websites.</p>
+        
+        <h3>7. Disclaimer of Warranties</h3>
+        <p>a. This Website is provided "as is," without any warranties, express or implied, including <br>
+        but not limited to the accuracy, reliability, or availability of the content.</p>
+        <p>b. The Company disclaims all liability for any errors, omissions, or inaccuracies in the <br>
+        content or for any loss or damage resulting from the use of this Website.</p>
+        
+        <h3>8. Limitation of Liability</h3>
+        <p>The Company and its affiliates shall not be liable for any direct, indirect, incidental, <br>
+            consequential, or punitive damages arising from your use or inability to use this Website.</p>
+        
+        <h3>9. Changes to Terms</h3>
+        <p>The Company reserves the right to modify these Terms at any time. It is your  <br>
+        responsibility to review these Terms periodically for updates. Your continued use of the  <br>
+        Website after any modifications signifies your acceptance of the revised Terms.</p>
+
+        <p>By using our Website, you consent to the terms outlined in this Privacy Policy.</p>
+
+        </div>
+
+        <br><br>
 
         <form onsubmit="login.agree_to_policies(event)" method="post" >
             <div class="terms_conditions" style="margin: 8px 0px 8px 0px;">
--- login_1.php
+++ login_1.php
@@ -154,8 +154,25 @@
             overflow-y: auto; 
         }
 
+        .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+
+        .content-modal {
+            margin-top:75px !important;
+        }
+
         /* The Close Button */
-        .close {
+        .close, .close-policies {
         color: black;
         float: right;
         font-size: 28px;
@@ -183,7 +200,7 @@
         }
 
         .close:hover,
-        .close:focus {
+        .close:focus, .close-policies:hover, .close-policies:focus {
         color: #000;
         text-decoration: none;
         cursor: pointer;
