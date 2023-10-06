3981ef8fcea4c01696ed8bb29c20b4b9d4c6d81c
laurent <laurentancheta15@gmail.com>
2023-10-07 02:10:40 +0800

    bug fix on load appoinments line 576

--- admin/ajax-admin.php
+++ admin/ajax-admin.php
@@ -538,6 +538,7 @@
             $offset = ($page - 1) * $rows_per_page;
             $column = $_POST['column'];
             $order = $_POST['order'];
+            $constant_total_rows = 0; // Initialize with a default value
             //$search_query = $_POST['search_query'];
             //echo $_POST['search_query'];
             if ($_POST['search_query'] == 'null') {
